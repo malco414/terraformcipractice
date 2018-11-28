92bf314ccabb9d56d776a48f8476ba30eec3376b
Saori <jkmr9214@gmail.com>
2018-11-28 18:33:18 +0900

    first commit

---
+++ modules/s3.tf
@@ -0,0 +1,23 @@
+
+resource "aws_s3_bucket" "bucket" {
+
+  bucket = "${var.bucket_name}"
+  acl    = "private"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowTest",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${var.aws_account_id}:root"
+      },
+      "Action": "s3:*",
+      "Resource": "arn:aws:s3:::${var.bucket_name}/*"
+    }
+  ]
+}
+POLICY
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_account_id" {}
+variable "region" {
+  default = "us-east-1"
+}
+variable "bucket_name" {
+  default = "jikumaru-sample-bucket"
+}
